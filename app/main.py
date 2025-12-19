581a7cd26aa09e645ec0a38d9ac4b39a341b012a
candrasw <candra.setiadi@machtwatch.co.id>
2025-12-19 09:56:12 +0700

    [ADD] handling cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from .database import engine, Base
 from .routers import stock_opname, inventory_movements
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # untuk dev: auto create table jika belum ada
 Base.metadata.create_all(bind=engine)
@@ -9,6 +11,19 @@ app = FastAPI(
     title="Smart Stock Opname RFID API",
     version="0.2.0",
 )
+
+# =========================
+# CORS CONFIGURATION
+# =========================
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=origins,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(stock_opname.router)
 app.include_router(inventory_movements.router)
